8ba7ab35d8e044d464f12b0ce36b15d955636f23
Alex Komlev <alexkomlev@sitecraft.io>
2022-09-13 14:47:28 +0300

    Testimonial SLider Update

--- wp-content/themes/regulora/app/Controllers/Modules/ModuleTestimonials.php
+++ wp-content/themes/regulora/app/Controllers/Modules/ModuleTestimonials.php
@@ -150,7 +150,7 @@ trait ModuleTestimonials
                 <?php if ( $testimonials ) : ?>
                     <div class="slider-container">
                         <div class="slider-inner">
-                            <div class="prev-but-place desktop d-none d-lg-block">
+                            <div class="prev-but-place desktop <?= ( $viewType === 'head' ? 'd-block mobile' : 'd-none d-lg-block' ) ?>">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M224 480c-8.188 0-16.38-3.125-22.62-9.375l-192-192c-12.5-12.5-12.5-32.75 0-45.25l192-192c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L77.25 256l169.4 169.4c12.5 12.5 12.5 32.75 0 45.25C240.4 476.9 232.2 480 224 480z"/></svg>
                             </div>
                             <div class="testimonials-slider">
@@ -166,14 +166,14 @@ trait ModuleTestimonials
                                     </div>
                                 <?php endforeach; ?>
                             </div>
-                            <div class="next-but-place desktop d-none d-lg-block">
+                            <div class="next-but-place desktop <?= ( $viewType === 'head' ? 'd-block mobile' : 'd-none d-lg-block' ) ?>">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z"/></svg>
                             </div>
-                            <div class="controll-buttons d-flex d-lg-none">
-                                <div class="prev-but-place mobile">
+                            <div class="controll-buttons <?= ( $viewType === 'head' ? 'd-none' : 'd-flex d-lg-none' ) ?>">
+                                <div class="prev-but-place <?= ( $viewType === 'head' ? '' : 'mobile' ) ?>">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M224 480c-8.188 0-16.38-3.125-22.62-9.375l-192-192c-12.5-12.5-12.5-32.75 0-45.25l192-192c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L77.25 256l169.4 169.4c12.5 12.5 12.5 32.75 0 45.25C240.4 476.9 232.2 480 224 480z"/></svg>
                                 </div>
-                                <div class="next-but-place mobile">
+                                <div class="next-but-place <?= ( $viewType === 'head' ? '' : 'mobile' ) ?>">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z"/></svg>
                                 </div>
                             </div>
