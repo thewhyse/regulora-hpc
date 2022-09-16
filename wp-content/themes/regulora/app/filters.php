188f9bbd34fff7125955efad70252453f216bc22
Alex Komlev <alexkomlev@sitecraft.io>
2022-09-17 00:29:13 +0300

    No cache SVG in cover block

--- wp-content/themes/regulora/app/filters.php
+++ wp-content/themes/regulora/app/filters.php
@@ -217,6 +217,14 @@ add_filter( 'render_block_core/search', function ( $block_content, $block )
 
 }, 10, 2 );
 
+add_filter( 'render_block_core/cover', function ( $block_content, $block )
+{
+    $block_content = str_replace( '.svg"', '.svg?t=' . time() . '"', $block_content );
+
+    return $block_content;
+
+}, 10, 2 );
+
 add_filter( 'pre_get_posts', function ( $query ) {
     if ( $query->is_search && !is_admin() ) {
         $query->set( 'meta_query', array(
